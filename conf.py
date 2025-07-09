0db932fd42e7b48ca37e61132caee2cc7c1e7fea
0ff-icesetup <110817241+0ff-icesetup@users.noreply.github.com>
2025-07-09 12:27:01 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Trust Wallet'
+project = 'Binance Support'
 
 
 # The full version, including alpha/beta/rc tags
